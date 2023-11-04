9d8d95b88d2f476716718ebd639dce79bfcb3ec8
Elvis <elvis.adomnica@gmail.com>
2023-11-04 02:43:28 +0100

    Cleanup rest of unnecessary files

--- index.js
+++
@@ -1,3 +0,0 @@
-const sum = require("./sum");
-
-console.log(sum(1, 2, 3, 4, 5));
--- sum.js
+++
@@ -1,7 +0,0 @@
-function sum(...args) {
-  return args.reduce((prev, cur) => {
-    return prev + cur;
-  }, 0);
-}
-
-module.exports = sum;
